74cd045517263c1003f97680b6e1ada79666ab0c
Kemi <92072045+kayelly@users.noreply.github.com>
2025-03-17 10:08:03 +0000

    Create init_database.sql

---
+++ scripts/init_database.sql
@@ -0,0 +1,11 @@
+WARNING: 
+    Running this script will drop the entire 'DataWarehouse' database if it exists.
+    All data in the database will be permanently deleted. Proceed with caution 
+    and ensure you have proper backups before running this script.
+
+*/
+
+USE master;
+GO 
+
+-- Drop and recreate the 'DataWarehouse' database 
